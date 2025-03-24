8300acecc52b09f6eacddba7556daabfe2a50ddb
Rahul <deshmukhrahul636@gmail.com>
2025-03-24 17:23:33 +0530

    postMapping

--- src/main/java/com/ecommerce/ecommerce/controller/CatogaryController.java
+++ src/main/java/com/ecommerce/ecommerce/controller/CatogaryController.java
@@ -2,6 +2,8 @@ package com.ecommerce.ecommerce.controller;
 
 import com.ecommerce.ecommerce.model.Catogary;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -15,5 +17,11 @@ public class CatogaryController {
     public List<Catogary> getAllCategories(){
         return categories;
     }
-
+    @PostMapping("/api/public/categories")
+public String CreateCatogery(@RequestBody Catogary cate){
+    if(categories.add(cate)){
+        return "Categories added successfully";
+    }
+    return "Categories not added";
+    }
 }
